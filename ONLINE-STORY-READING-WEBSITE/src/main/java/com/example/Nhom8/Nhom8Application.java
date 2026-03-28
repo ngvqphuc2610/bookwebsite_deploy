ae26bd61a90a0d705ca6fa57f153832af5cb0b09
ngvqphuc2610 <nguyenvqphuc@gmail.com>
2026-03-29 00:32:21 +0700

    add database seeder

--- ONLINE-STORY-READING-WEBSITE/src/main/java/com/example/Nhom8/Nhom8Application.java
+++ ONLINE-STORY-READING-WEBSITE/src/main/java/com/example/Nhom8/Nhom8Application.java
@@ -15,22 +15,4 @@ public class Nhom8Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Nhom8Application.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner initData(RoleRepository roleRepository, FaqItemRepository faqItemRepository) {
-		return args -> {
-			if (roleRepository.findByName("USER").isEmpty()) {
-				roleRepository.save(Role.builder().name("USER").description("Reader").build());
-			}
-			if (roleRepository.findByName("STAFF").isEmpty()) {
-				roleRepository.save(Role.builder().name("STAFF").description("Editor").build());
-			}
-			if (roleRepository.findByName("ADMIN").isEmpty()) {
-				roleRepository.save(Role.builder().name("ADMIN").description("Administrator").build());
-			}
-			if (faqItemRepository.count() == 0) {
-				faqItemRepository.saveAll(CustomerCareService.buildDefaultFaqs());
-			}
-		};
-	}
 }
